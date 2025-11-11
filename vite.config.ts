473a888710e0bd3f8184169c939e227594b22a6f
hasan <istamqulov@gmail.com>
2025-11-12 03:01:47 +0500

    new design

--- vite.config.ts
+++ vite.config.ts
@@ -97,6 +97,18 @@ export default defineConfig({
         target: 'http://localhost:8000',
         changeOrigin: true,
       },
+      '/admin': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+      '/static': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+      '/media': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
     },
   },
   build: {
